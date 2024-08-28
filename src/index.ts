58bcf45d3b6ed20828f7c17d011943237ccca0d0
crewbloomrob <robert@usebetty.ai>
2024-08-28 08:04:54 -0400

    chore: update GET / request response

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ const expressApp = express();
 console.log('environment', environment);
 
 expressApp.get('/', function (req, res) {
-  res.send('Hello World');
+  res.status(200).json({
+    message:'Success'
+  })
 });
 
 expressApp.listen(PORT, () => {
